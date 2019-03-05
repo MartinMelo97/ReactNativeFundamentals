9b52b6a70350d4c06ab61ae11c15a4103f22a6d7
Martin Melo <nitram-210397@hotmail.com>
2019-03-05 00:43:46 -0600

    Creating a dummie data, and Quiz Detail is done

---
+++ app/data/QuizQuestions.js
@@ -0,0 +1,31 @@
+export const QuizData = {
+    questions: [
+        {
+            question: "What language do you use with libraries such as React and Angular?",
+            answer1: 'PHP',
+            answer2: 'JAVA',
+            answer3: 'JavaScript',
+            answer4: 'Python',
+            correctAnswer: 'answer3',
+            key: '1A'
+        },
+        {
+            question: "Howdo you indicate values are not equal in Javascript?",
+            answer1: '===',
+            answer2: '=>',
+            answer3: '!==',
+            answer4: '<=',
+            correctAnswer: 'answer3',
+            key: '1B'
+        },
+        {
+            question: "Which one of these is a way yo store multiple values in a single variable?",
+            answer1: 'string',
+            answer2: 'array',
+            answer3: 'number',
+            answer4: 'boolean',
+            correctAnswer: 'answer2',
+            key: '1C'
+        },
+    ]
+}
